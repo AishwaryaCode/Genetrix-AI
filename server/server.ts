b3cf8604ea7189b22d5b9fe384945f8c820a5bae
Aishwarya <aishwaryapatil1120@gmail.com>
2026-02-24 16:32:21 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -30,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie:{maxAge: 1000 *60 * 60 * 24 *7, 
             secure:true,
-           sameSite:"none"}, 
+           sameSite:'none'}, 
     store: MongoStore.create({
         mongoUrl : process.env.MONGODB_URL as string,
         collectionName: 'sessions'
